69e9f8857ef13beb6a90e5923b67b0fb9769e2a0
Jack-w-DOS <jack_w@directonlineservices.co.uk>
2020-03-25 21:27:04 +0000

    Project setup

---
+++ src/Components/Navigation/Topnav/Topnav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const topnav = (props) => {
+
+    return (
+        <nav className="nav">
+            <div className="nav__head">Bandapp</div>
+            <div className="nav__menu">Menu</div>
+        </nav>
+    )
+}
+
+export default topnav
---
+++ src/Containers/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Topnav from '../../Components/Navigation/Topnav/Topnav'
+
+class Layout extends Component {
+    render() {
+        return (
+            <>
+                <Topnav /> 
+            </>
+        )
+    }
+}
+
+export default Layout
